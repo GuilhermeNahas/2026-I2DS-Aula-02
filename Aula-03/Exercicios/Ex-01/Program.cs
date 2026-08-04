fa95147624e997ee73f5b7eba8b9d34f6f0df821
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-04 16:20:29 -0300

    Iniciando os exercios de C#

---
+++ Aula-03/Exercicios/Ex-01/Program.cs
@@ -0,0 +1,12 @@
+﻿// EX 1 Criar um programa capaz de converter temperaturas entre diferentes unidades.
+//Fahrenheit = (Celsius * 9 / 5) + 32
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(" Digite a temperatura em °C: ");
+        
+
+    }
+}
