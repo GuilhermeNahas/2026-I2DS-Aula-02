e12c09e26bf4c13624a386048d3c9a105e5f9731
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-05 08:39:18 -0300

    Ex-06

---
+++ Aula-03/Exercicios/Ex-05/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+1. Cadastrar produtos;
+2. Cadastrar preços;
+3. Listar produtos cadastrados;
+4. Pesquisar um produto pelo nome.
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+
+
+    }
+}
