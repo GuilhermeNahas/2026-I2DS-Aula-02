e275bf74d12c0c9f7811d18332026982a41a56dc
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-05 09:16:34 -0300

    Ex-09 (Cristiano)

---
+++ Aula-03/Exercicios/Ex-08/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+1. Solicitar usuário;
+2. Solicitar senha;
+3. Permitir até três tentativas;
+4. Bloquear após três erros.
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string  usuario = "admin";
+        const string senha = "1234";
+
+        int tentativas = 0;
+        const int maxTentativas = 3;
+        bool loginSucesso = false;
+
+        while (tentativas < maxTentativas && !loginSucesso)
+        {
+            Console.WriteLine($"--- Login (Tentativa {tentativas + 1} de {maxTentativas}) ---");
+        }
+
+    }
+}
