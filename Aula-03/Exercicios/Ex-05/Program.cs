f49e956d21e97f4855db7660b15e22c62e4a9864
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-05 09:23:49 -0300

    Ex-05

--- Aula-03/Exercicios/Ex-05/Program.cs
+++ Aula-03/Exercicios/Ex-05/Program.cs
@@ -6,11 +6,63 @@
 */
 internal class Program
 {
-    private static void Main(string[] args)
+ private static void Main(string[] args)
     {
-        
+        List<string> produtos = new List<string>();
+        List<double> precos = new List<double>();
+        string opcao = "";
 
+        while (opcao != "5")
+        {
+            Console.Clear();
+            Console.WriteLine("1. Cadastrar produto");
+            Console.WriteLine("2. Cadastrar preco");
+            Console.WriteLine("3. Listar produtos");
+            Console.WriteLine("4. Pesquisar produto");
+            Console.WriteLine("5. Sair");
+            Console.Write("Escolha uma opcao: ");
+            opcao = Console.ReadLine();
 
+            switch (opcao)
+            {
+                case "1":
+                    Console.Write("Nome do produto: ");
+                    string nome = Console.ReadLine();
+                    break;
 
+                case "2":
+                    Console.Write("Preco do produto: ");
+                    double preco = double.Parse(Console.ReadLine());
+                    break;
+
+                case "3":
+                    for (int i = 0; i < produtos.Count; i++)
+                    {
+                        Console.WriteLine($"Produto: {produtos[i]} - Preco: R$ {precos[i]}");
+                    }
+                    break;
+
+                case "4":
+                    Console.Write("Digite o nome para pesquisar: ");
+                    string busca = Console.ReadLine();
+                    int indice = produtos.IndexOf(busca); //procura pelo conteúdo da variável busca dentro da lista produtos
+
+                    if (indice != -1)
+                    {
+                        Console.WriteLine($"Encontrado! Produto: {produtos[indice]} - Preco: R$ {precos[indice]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Produto nao encontrado.");
+                    }
+                    break;
+            }
+
+            if (opcao != "5")
+            {
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
     }
 }
