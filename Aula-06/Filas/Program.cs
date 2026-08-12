53cf84c86d1569949c476db5d17ad84954c7fa22
GuilhermeNahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-12 11:14:13 -0300

    Aula 06- Apresentando elementos de filas, listas, pilhas e dicionarios

---
+++ Aula-06/Filas/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM FILAS (FIFO) ===");
+
+
+        // 1 - Criar uma nova fila vazia
+        Queue<string> filaBanco = new Queue<string>();
+
+        // 2 - Adicionar elementos na fila
+        filaBanco.Enqueue("Zuleika"); // Indice 0
+        filaBanco.Enqueue("Asdrubal"); // Indice 1
+        filaBanco.Enqueue("Pafuncio");  // Indice 2
+        filaBanco.Enqueue("Epaminondas"); // Indice 3
+
+        // 3 - Percorrer elementos de uma fila
+        Console.WriteLine();
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // 4 - Remover elementos de uma fila
+        string pessoaRetirada = filaBanco.Dequeue();
+        Console.WriteLine();
+        Console.WriteLine($"O nome chamado foi: {pessoaRetirada}");
+
+        filaBanco.Enqueue("Lucrécia");
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // 5 - Verificar se existe  um determinado elemento na lista
+        string procurar = "Pafuncio";
+        bool achou = filaBanco.Contains(procurar);
+        Console.WriteLine();
+
+        if (achou)
+        {
+            Console.WriteLine($"{procurar} está na fila!");
+        }
+        else
+        {
+            Console.WriteLine($"{procurar} NAO esta na fila");
+        }
+    }
+}
---
+++ Aula-06/Pilhas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM PILHAS (FILO)===");
+
+        // 1 - Criar uma nova pilha vazia 
+        Stack<string> pilhaLivros = new Stack<string>();
+
+        // 2 - Adicionar elementos em uma pilha
+        pilhaLivros.Push("Dom Quixote");
+        pilhaLivros.Push("O Morro dos Ventos Uivantes");
+        pilhaLivros.Push("O Cortico");
+
+        // 3 - Percorrer todos os elementos de uma pilha 
+        foreach (var item in pilhaLivros)
+        {
+            Console.WriteLine(item); //as 2 variaveis devem ser iguais -> item && item
+        }
+
+        // 4 - Remover um elemento da pilha 
+        string livroRemovido = pilhaLivros.Pop(); //.Pop -> Remove o primeiro item da lista
+        Console.WriteLine();
+        Console.WriteLine($"O livro retirado foi: {livroRemovido}");
+
+        pilhaLivros.Push("Dom Casmurro");
+        Console.WriteLine();
+        foreach (var livro in pilhaLivros)
+        {
+            Console.WriteLine(livro);
+        }
+    }
+}
