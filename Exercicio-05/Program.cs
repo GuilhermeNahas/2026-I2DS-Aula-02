4003f0854d27db2cf1a7503420157c11ef7dc833
Guilherme Gimenez Nahas <guilhermegimeneznahas.senai@gmail.com>
2026-08-17 09:04:27 -0300

    Lista de Exercicios

---
+++ Exercicio-05/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+1. Empilhe 4 ações;
+2. Desempilhe (`Pop`) as 2 últimas ações;
+3. A cada `Pop`, imprima qual ação foi desfeita.
+
+**Conceitos:** `Stack<T>` • `Push()` • `Pop()`
+*/
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<string> historico = new Stack<string>();
+        historico.Push("Digitar");
+        historico.Push("Colar");
+        historico.Push("Negrito");
+        historico.Push("Apagar");
+
+        string desfazer1 = historico.Pop();
+        Console.WriteLine($"Ação desfeita: {desfazer1}");
+
+        string desfazer2 = historico.Pop();
+        Console.WriteLine($"Ação desfeita: {desfazer2}");
+
+    }
+}
---
+++ Exercicio-06/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+A função deve calcular a **multiplicação entre dois números**.
+
+Teste a função utilizando **3 pares de valores diferentes** e imprima os resultados no console.
+**Conceitos:** `Func<>` • Lambda Expression
+
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
